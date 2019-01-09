b41aaf19813ca9dfd4fe3f55325d4d7eaf920b4f
Chris Woolf <chriswoolf@mail.com>
2019-01-09 05:14:56 +0000

    aded touch frame

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,190 +1,106 @@
 
-       /* var canvas = document.getElementById("inner_heading-canvas");
+    var sizes = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 70, 80, 90, 100];
 
-        var mousePos = {
-            x: 0,
-            y: 0
-        };
-
-        if (canvas.getContext) {
-            var context = canvas.getContext("2d");
-        }
-
-        window.addEventListener('load', init, false);
-
-        window.addEventListener('mousemove', printMouse, false);
-      window.addEventListener('touchmove', printMouse, false);
+    function shuffle(array) {
+      var randomNum = Math.floor(Math.random() * array.length);
+      return array[randomNum];
+    }
+    
+    var myDiv = document.getElementById('myDiv');
+    
+    myDiv.addEventListener('touchstart', function(e) {
+      bubbles(e);
+    });
+    
+    myDiv.addEventListener('touchmove', function(e) {
+     bubbles(e);
+    });
+    
+    function bubbles(e) {
+        e.preventDefault();
+      
+      var touch = e.changedTouches[0];
+      var posX = touch.clientX - 25;
+      var posY = touch.clientY - 25;
+      
+      var span = document.createElement('span');
+      span.style.left = posX + 'px';
+      span.style.top = posY + 'px';
+      
+      var bubbleSize = shuffle(sizes);
+      var num = Math.floor(Math.random() * 2);
+      var zIn = Math.floor(Math.random() * 11);
+      
+      if(bubbleSize < 30) {
+        span.style.border = 1 + 'px solid white';
+        if(num && zIn == 0) span.setAttribute('class', 'blur');
+      }
+      else if(bubbleSize < 40) {
+        span.style.border = 2 + 'px solid white';
+      }
+      else if(bubbleSize < 50) {
+        span.style.border = 3 + 'px solid white';
+      }
+      else if(bubbleSize < 60) {
+        span.style.border = 4 + 'px solid white';
+      }
+      else if(bubbleSize < 70) {
+        span.style.border = 5 + 'px solid white';
+      }
+      else if(bubbleSize < 80) {
+        span.style.border = 6 + 'px solid white';
+        if(num && zIn > 8) span.setAttribute('class', 'blur');
+      }
+      else if(bubbleSize < 90) {
+        span.style.border = 7 + 'px solid white';
+      }
+      else if(bubbleSize <= 100) {
+        span.style.border = 8 + 'px solid white';
+      }
+      
+      if(num == 1) {
+        span.style.transform = 'scaleZ(' + zIn + ')';
+        span.style.zIndex = zIn;
+      }
+      else if (num == 0) {
+        span.style.transform = 'scaleZ(-' + zIn + ')';
+        span.style.zIndex = '-' + zIn;
+      }
+      span.style.height = bubbleSize + 'px';
+      span.style.width = bubbleSize + 'px';
+      myDiv.appendChild(span);
+      
+      var randomNum = Math.floor(Math.random() * 1000) + 50;
+      var floatTo = posY - randomNum;
+      
+      var removeSpeed = Math.floor(Math.random() * 5000) + 500;
+      
+      setTimeout(function() {
+        span.style.top = floatTo + 'px';
         
-        var timeoutID = null;
-        var rafID = null;
-        
-        window.addEventListener("resize", function() {
-            clearTimeout(timeoutID);
-            window.cancelAnimationFrame(rafID);
-            timeoutID = setTimeout(init, 500);
-        }, false);
+        setTimeout(function() {
+          floating(span, floatTo);
+          setTimeout(function() {
+            span.remove();
+          }, removeSpeed)
+        }, 1700);
+      }, 0);
+    }
+    
+    function floating(span, floatTo) {
+      span.style.top = (floatTo + 10) + 'px';
+      setTimeout(function() {
+        span.style.top = (floatTo - 10) + 'px';
+        setTimeout(function() {
+          span.style.top = (floatTo + 10) + 'px';
+          setTimeout(function() {
+            span.style.top = (floatTo - 10) + 'px';
+          }, 1700);
+        }, 1700);
+      }, 1700);
+    }
 
 
-        // Init
-        function init() {  
-            var net = undefined;
-
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-
-            var nodesLength = Math.floor(canvas.width*canvas.height/3000);
-            
-            // Nodes
-            net = new Net();
-            net.populate(nodesLength);
-
-            rafID = window.requestAnimationFrame(render);
-
-            function render(e) {
-                net.update();
-                net.draw();
-                net.connect(120);
-                rafID = window.requestAnimationFrame(render);
-            }
-        }
-
-
-        // Net
-        class Net {
-            constructor() {
-                this.nodes = [];
-                this.length = undefined;
-            }
-
-            populate(length) {
-                this.length = length;
-                
-                for (var i = 0; i < length; i++) {
-                    var xPos = Math.floor(getRandom(0, canvas.width));
-                    var yPos = Math.floor(getRandom(0, canvas.height));
-                    this.nodes.push(new Node(xPos, yPos));
-                }
-            }
-
-            update() {
-                for (var i = 0; i < this.length; i++) {
-                    this.nodes[i].update();
-                }
-            }
-
-            draw() {
-                context.fillStyle = "#0E0E28";
-                context.fillRect(0, 0, canvas.width, canvas.height);
-                for (var i = 0; i < this.length; i++) {
-                    this.nodes[i].draw();
-                }
-
-            }
-
-            connect(distanceMax) {
-                // Loop through all nodes
-                for (var i = 0; i < this.length - 1; i++) {
-                    this.nodes[i].connections = [];
-                    
-                    // Store connected nodes in node object
-                    for (var j = 0; j < this.length - 1; j++) {
-                        var a = this.nodes[j].x - this.nodes[i].x;
-                        var b = this.nodes[j].y - this.nodes[i].y;
-                        var c = Math.sqrt(a*a + b*b);
-
-                        var xToMouse = this.nodes[j].x - mousePos.x;
-                        var yToMouse = this.nodes[j].y - mousePos.y;
-                        this.nodes[i].dToMouse = Math.floor(Math.sqrt(xToMouse*xToMouse + yToMouse*yToMouse));
-
-                        var d = distanceMax/this.nodes[i].dToMouse*100;;
-
-                        if (distanceMax/this.nodes[i].dToMouse*200 > distanceMax) {
-                            d = distanceMax;
-                        }
-                        
-                        if (j > i && c < d) {
-                            this.nodes[i].connections.push(j);
-                        }
-                    }
-
-                    // Draw line between dots
-                    for (var k = 0; k < this.nodes[i].connections.length; k++) {
-                        context.beginPath();
-                        context.moveTo(this.nodes[i].x, this.nodes[i].y);
-                        context.lineTo(this.nodes[this.nodes[i].connections[k]].x, this.nodes[this.nodes[i].connections[k]].y);
-                        context.strokeStyle = "rgba(211,211,211,"+ this.nodes[i].depth/8 +")";
-                        context.stroke();
-                    }
-                }
-            }
-        }
-
-
-        // Node
-        class Node {
-            constructor(_x, _y) {
-                this.x = _x;
-                this.y = _y;
-                this.radius = 2;
-                this.depth = Math.floor(getRandom(1, 20))/10;
-            }
-
-            update() {
-                var velocity = (1 - this.depth)/5;
-                this.x = this.x + velocity;
-
-                if (this.x > canvas.width || this.x < 0) {
-                    this.x = 0;
-                }
-            }
-
-            draw() {
-                var alpha = 1 - this.depth;
-                context.beginPath();
-                context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-                context.fillStyle = 'rgba(211,211,211,' + alpha + ')';
-                context.fill();
-            }
-        }
-
-
-        // Helpers
-        function getRandom(min, max) {
-          return Math.random() * (max - min) + min;
-        }
-
-        // Mouse
-        
-        function getMousePos(canvas, evt) {
-          var rect = canvas.getBoundingClientRect(), // abs. size of element
-              scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
-              scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
-
-          return {
-            x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-            y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
-          }
-        }
-
-        function printMouse(e) {
-            var c = canvas;
-            var pos = getMousePos(c, e);
-            mousePos = pos;
-        }
-
-        // Stats
-        var stats = new Stats();
-		stats.showPanel(0);
-		document.body.appendChild(stats.dom);
-
-		function animate() {
-			stats.begin();
-			stats.end();
-
-			requestAnimationFrame(animate);
-		}
-
-        requestAnimationFrame(animate); */
         'use strict';
 // Abandon all hope ye who enter here
 $(document).ready(function() {
