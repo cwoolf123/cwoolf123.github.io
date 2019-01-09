1c030d6327c181174ffe04d888175ae9df5fdfa7
Chris Woolf <chriswoolf@mail.com>
2019-01-09 14:01:40 +0000

    more pages added and some title changes

---
+++ about/js/title.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+
+	var blue = '#2980b9';
+	var l = Snap('#logo');
+	var p = l.select('path');
+
+	setTimeout( function() {
+		// modify this one line below, and see the result !
+		var logoTitle = 'About';
+		var logoRandom = '';
+		var logoTitleContainer = l.text(0, '100%', '');
+		var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
+		logoTitleContainer.attr({
+			fontSize: 140,
+			fontFamily: 'Comfortaa',
+			fontWeight: '800'
+		});
+    
+		function generateRandomTitle(i, logoRandom) {
+			setTimeout( function() {
+				logoTitleContainer.attr({ text: logoRandom });
+			}, i*125 );
+		}
+
+		for( var i=0; i < logoTitle.length+1; i++ ) {
+			logoRandom = logoTitle.substr(0, i);
+			for( var j=i; j < logoTitle.length; j++ ) { 
+				logoRandom += possible.charAt(Math.floor(Math.random() * possible.length)); 
+			}
+			generateRandomTitle(i, logoRandom);
+			logoRandom = '';
+		}
+
+	}, 500 );
+
+	
+}
