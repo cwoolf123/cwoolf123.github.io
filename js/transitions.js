6c6e0406076f8764a55e81b9cc673c5a881ee20c
Chris Woolf <CWoolf@penguinrandomhouse.co.uk>
2019-10-14 23:58:38 +0100

    hexagon redesign for portfolio

--- js/transitions.js
+++ js/transitions.js
@@ -1,18 +1,21 @@
 $("nav a").on("click", function (event) {
+//If mobile do this...
+  if ( window.innerWidth < 767 ) {
+    $('nav').animate({ 
+      opacity: 0,
+      top: "-20em",
+      zIndex: 1,
+  }, 500 );
+  
   $('.line1').animate({ 
     width: "2rem",
-}, 200 );
-$('.line2').animate({ 
+  }, 200 );
+  $('.line2').animate({ 
     width: "2rem",
-}, 400 );
-$('.line3').animate({ 
+  }, 400 );
+  $('.line3').animate({ 
     width: "2rem",
-}, 600 );
-  $('nav').animate({ 
-    opacity: 0,
-    top: "-20em",
-    zIndex: 1,
-}, 500 );
+  }, 600 );
   
     event.preventDefault()
     
@@ -36,7 +39,41 @@ $('.line3').animate({
         })
       }
     })
+    //Otherwiae do this...
+  } else {
+    $('.line1').animate({ 
+      width: "2rem",
+    }, 200 );
+    $('.line2').animate({ 
+      width: "2rem",
+    }, 400 );
+    $('.line3').animate({ 
+      width: "2rem",
+    }, 600 );
     
+      event.preventDefault()
+      
+      const href = $(this).attr("href")
+      
+      window.history.pushState(null, null, href)
+      
+      $("nav a").removeClass("active")
+      $(this).addClass("active")
+      
+      $.ajax({
+        url: href,
+        success: function (data) {
+          $("section").fadeOut(250, function () {
+            const newPage = $(data).filter("section").html()
+            
+            $("section").html(newPage)
+            
+            $("section").fadeIn(250)
+   
+          })
+        }
+      })
+    }
   })
 
   
